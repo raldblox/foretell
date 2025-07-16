bf09d020978d2a1051c7450216d7626862a9a911
raldblox <raldblox@outlook.com>
2025-07-17 04:18:41 +0800

    added: reward table

---
+++ foretell-app/components/reward-table.tsx
@@ -0,0 +1,63 @@
+"use client";
+import {
+  Spinner,
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@heroui/react";
+
+export interface RewardEntry {
+  uid: string;
+  answer?: string;
+  polarity: number;
+  score: number;
+  pctShare: number;
+  rewardUSD: number;
+}
+
+interface RewardTableProps {
+  data: RewardEntry[];
+  isLoading?: boolean;
+}
+
+export function RewardTable({ data, isLoading = false }: RewardTableProps) {
+  return (
+    <Table
+      isHeaderSticky
+      aria-label="Rewards Table"
+      classNames={{ base: "max-h-[520px] overflow-auto", table: "min-w-full" }}
+    >
+      <TableHeader>
+        <TableColumn key="uid">UID</TableColumn>
+        <TableColumn key="answer">Response</TableColumn>
+        <TableColumn key="polarity">Polarity</TableColumn>
+        <TableColumn key="score">Intensity</TableColumn>
+        <TableColumn key="pctShare">Closeness</TableColumn>
+        <TableColumn key="rewardUSD">Rewards</TableColumn>
+      </TableHeader>
+
+      <TableBody
+        items={data}
+        isLoading={isLoading}
+        loadingContent={<Spinner color="white" />}
+      >
+        {(item) => (
+          <TableRow key={item.uid} className="hover:bg-default-100">
+            {(columnKey) => (
+              <TableCell>
+                {columnKey === "pctShare"
+                  ? `${item.pctShare.toFixed(1)}%`
+                  : columnKey === "rewardUSD"
+                    ? `${item.rewardUSD.toFixed(4)}`
+                    : (item as any)[columnKey]}
+              </TableCell>
+            )}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
+}
