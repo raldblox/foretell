f3f5a39462d6b1e803ef8e29acb2e5ac32152fae
raldblox <raldblox@outlook.com>
2025-07-19 21:49:37 +0800

    update footer

--- foretell-app/components/footer.tsx
+++ foretell-app/components/footer.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full z-50 flex pt-12 border-t bg-background/50 border-default-100 items-center justify-center py-6">
+    <footer className="w-full z-50 flex border-t bg-background/50 border-default-100 items-center justify-center py-6">
       <Link
         isExternal
         className="flex items-center gap-1 font-bold text-default-800"
