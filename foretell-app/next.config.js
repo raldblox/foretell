bad881b155bbbdd834e0ef445eab95da0288d48a
raldblox <raldblox@outlook.com>
2025-07-19 14:53:28 +0800

    added: farcaster manifest

--- foretell-app/next.config.js
+++ foretell-app/next.config.js
@@ -11,6 +11,16 @@ const nextConfig = {
     });
     return config;
   },
+  async redirects() {
+    return [
+      {
+        source: "/.well-known/farcaster.json",
+        destination:
+          "https://api.farcaster.xyz/miniapps/hosted-manifest/01982170-96e8-8ba5-92ba-6d98d6dff0f8",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
